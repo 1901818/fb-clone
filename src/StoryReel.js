cc70435489f9e1e9c037c8ceb92d6f23c224fbff
AndrewAdulla <adulladrew@gmail.com>
2021-01-17 00:03:16 -0800

    storyreel fixed

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,33 +1,35 @@
 import React from "react";
-import Story from "./Story";
 import "./StoryReel.css";
+import Story from "./Story";
 
-function StoryReel() {
-  return( 
+export default function StoryReel() {
+  return (
     <div className="storyReel">
       <Story
-        img src ="/public/child.png" alt=""
-        profileSrc="https://www.nicepng.com/png/full/115-1150176_employee-avatar-png-transparent-image-avatar-male.png"
-        title ="Andrew"
+        profileSrc="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        image="https://images.unsplash.com/photo-1599738001738-ab67bf681c07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2501&q=80"
+        title="Him k"
       />
       <Story
-        image="https://lh3.googleusercontent.com/proxy/yJHdN0AsTe50SpzyC2jsH_X34TicmTjzgfOKvxZaBSYUAddzvMyqFeTCQodlU5rakEh8WSYKA5oSUn9Oad0kJUM09YiCh6yRWTfQaE05qG3mw9g"
-        profileSrc="https://www.pngarts.com/files/3/Avatar-PNG-Photo.png"
-        title="Adulla"
+        profileSrc="https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70"
+        image="https://images.unsplash.com/photo-1599715939861-36c598ae5568?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        title="adulla andrew"
       />
       <Story
-        image="https://lh3.googleusercontent.com/proxy/_lXkOm8L-2mngAIa8o_uukMsKqVUJHmKZujGGSeKRYRdeEarb2ZeKor1oYhXM59-JF1hUUTph82ebuUGvnRmlIHBD1OLOyBfi76_hmoTGg2fTBOJCKcpGoWd80Xn6wn99Fk"
-        profileSrc="https://img.favpng.com/0/19/12/computer-icons-avatar-icon-design-png-favpng-MbC2sNWBjav8k1nYeY0FMTcgZ.jpg"
-        title="Ajali"
+        profileSrc="https://miro.medium.com/max/2048/0*0fClPmIScV5pTLoE.jpg"
+        image="https://images.unsplash.com/photo-1599701834133-9ae09fcfa601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+        title="jack john"
       />
       <Story
-        image="https://lh3.googleusercontent.com/proxy/_lXkOm8L-2mngAIa8o_uukMsKqVUJHmKZujGGSeKRYRdeEarb2ZeKor1oYhXM59-JF1hUUTph82ebuUGvnRmlIHBD1OLOyBfi76_hmoTGg2fTBOJCKcpGoWd80Xn6wn99Fk"
-        profileSrc="https://cdn.icon-icons.com/icons2/35/PNG/512/boss_person_man_people_avatar_2858.png"
-        title="Nanzi"
+        profileSrc="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg"
+        image="https://images.unsplash.com/photo-1599709513174-6309fb9c0a8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        title="flavina"
       />
-
+      <Story
+        profileSrc="https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+        image="https://images.unsplash.com/photo-1599744317651-f1f6863d95ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80"
+        title="lovely me"
+      />
     </div>
   );
 }
-
-export default StoryReel;
